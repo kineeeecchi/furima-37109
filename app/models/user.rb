449e7a4ba4e5390e161611a7b43015be87444ef1
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-08 22:55:50 +0900

    バリデーション一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,13 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX, message: 'には半角で英字と数字を含めてください'}
   
-  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
   with_options presence: true, format: { with: NAME_REGEX, message: 'は全角文字を使用してください' } do
     validates :last_name
     validates :first_name
   end
 
-  NAME_KANA_REGEX = /\A[ァ-ヶ一]+\z/
+  NAME_KANA_REGEX = /\A[ァ-ヶ一]+\z/.freeze
   with_options presence: true, format: { with: NAME_KANA_REGEX, message: 'は全角カタカナを使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
