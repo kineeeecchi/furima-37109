1d70b6f05bb4c9ba5ee461d51936044efe038714
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-08 19:40:09 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には半角で英字と数字を含めてください'}
+  
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  with_options presence: true, format: { with: NAME_REGEX, message: 'は全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  NAME_KANA_REGEX = /\A[ァ-ヶ一]+\z/
+  with_options presence: true, format: { with: NAME_KANA_REGEX, message: 'は全角カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birthday, presence: true
+
 end
