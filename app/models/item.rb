69bc1db8d6a0832b4787f850196ceedcb2bad44e
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-10 17:58:11 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_prefecture
+  belongs_to :shipping_schedule
+
   belongs_to :user
   has_one_attached :image
+
+  
 end
