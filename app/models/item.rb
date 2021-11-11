29a91c8c8773ff3a3763fe850c904c469304e995
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-12 08:11:43 +0900

    出品登録実装とバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,5 +9,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_info
+    validates :price, numericality: { only_integer: true, message: "は半角数値で入力"}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は¥300~¥9,999,999の間で入力" }
+  end
+  validates :category_id, :item_condition_id, :shipping_fee_id, :shipping_prefecture_id, :shipping_schedule_id, numericality: { other_than: 1 , message: "can't be blank"}
   
 end
