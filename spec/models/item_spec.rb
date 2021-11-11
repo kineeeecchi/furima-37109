dea127057aeb12cd989f3665f314bea881268a86
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-12 08:12:04 +0900

    itemモデルテストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+describe '商品出品登録' do
+  context '出品登録できるとき' do
+    it 'image~priceまで全てが存在すれば登録できる' do
+      expect(@item).to be_valid
+    end
+  end
+  context '出品登録できないとき' do
+    it 'imageが空では登録できない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+    it 'item_nameが空では登録できない' do
+      @item.item_name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item name can't be blank")
+    end
+    it 'item_infoが空では登録できない' do
+      @item.item_info = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item info can't be blank")
+    end
+    it 'category_idが未選択だと登録できない' do
+      @item.category_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+    it 'item_condition_idが未選択だと登録できない' do
+      @item.item_condition_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Item condition can't be blank")
+    end
+    it 'shipping_fee_idが未選択だと登録できない' do
+      @item.shipping_fee_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+    end
+    it 'shipping_prefecture_idが未選択だと登録できない' do
+      @item.shipping_prefecture_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping prefecture can't be blank")
+    end
+    it 'shipping_schedule_idが未選択だと登録できない' do
+      @item.shipping_schedule_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping schedule can't be blank")
+    end
+    it 'priceが空では登録できない' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+    it 'priceは半角数字でないと登録できない' do
+      @item.price = "１０００"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price は半角数値で入力")
+    end
+    it 'priceは¥300以上でないと登録できない' do
+      @item.price = 100
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price は¥300~¥9,999,999の間で入力")
+    end
+    it 'priceは¥300~¥9,999,999の間でないと登録できない' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price は¥300~¥9,999,999の間で入力")
+    end
+  end
+end
+
 end
