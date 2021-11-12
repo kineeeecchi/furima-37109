288cd02e1332a14197c886cff18ccf7376708f8f
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-12 18:30:58 +0900

    販売手数料と利益の表示機能修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,16 +4,13 @@ window.addEventListener('load', function(){
 
   itemPrice.addEventListener("input", () => {
     const inputValue = itemPrice.value;
-    console.log(inputValue);
 
     const addTaxPrice = document.getElementById("add-tax-price")
     addTaxPrice.innerHTML = inputValue * 0.1
-    console.log(addTaxPrice);
 
     const itemProfit = document.getElementById("profit")
     const taxValue = inputValue * 0.1
     itemProfit.innerHTML = (Math.floor(inputValue - taxValue));
-    console.log(itemProfit);
 
   })
 })
