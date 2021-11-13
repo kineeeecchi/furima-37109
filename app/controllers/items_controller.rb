7760e90e0116d837bfbe2774e06617c5b0c93a62
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-13 22:05:32 +0900

    Rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,8 @@ class ItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id])
     if user_signed_in? && current_user.id == item.user_id
-    item.destroy
-    redirect_to root_path
+      item.destroy
+      redirect_to root_path
     end
   end
 
